55530cdc7e51682295f48cc1bc06c4e19efc9948
abhay.patidar@zehntech.com-at-510548384854 <abhay.patidar@zehntech.com-at-510548384854>
2021-07-02 12:26:35 +0530

    Initial commit

---
+++ lib/src/modules/home/ui/home.dart
@@ -0,0 +1,34 @@
+import 'package:app/src/locale/app_translations.dart';
+import 'package:app/src/modules/home/ui/drawer.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () {} as Future<bool> Function()?,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('home'),
+        ),
+        drawer: AppDrawer(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Home'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
