50da03f12c02221c7f0e33d5f2c30dd806c82860
App Developer <pshrathod96@gmail.com>
2021-11-18 18:50:43 +0530

    General changes

--- lib/src/modules/news/bloc/news_bloc.dart
+++ lib/src/modules/news/bloc/news_bloc.dart
@@ -37,6 +37,8 @@ class NewsBloc extends Bloc<NewsEvent, NewsState> {
 
   Future<List<NotificationList>> fetchNotificationList() async {
     try {
+      print("https://onesignal.com/api/v1/notifications?app_id=${Overrides.PUSH_APP_ID}");
+      print('Basic ${Overrides.REST_API_KEY}');
       final response = await http.get(
           Uri.parse(
               "https://onesignal.com/api/v1/notifications?app_id=${Overrides.PUSH_APP_ID}"),
