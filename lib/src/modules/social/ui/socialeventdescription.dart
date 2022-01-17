31a74423c7fc47405207b26bc35ce0c42eca1242
Devesh Gupta <50574293+ztdg0068@users.noreply.github.com>
2022-01-17 15:48:44 +0530

    pull

--- lib/src/modules/social/ui/socialeventdescription.dart
+++ lib/src/modules/social/ui/socialeventdescription.dart
@@ -49,36 +49,36 @@ class SocialDescription extends StatelessWidget {
     this.currentindex++;
   }
 
-  void htmlparser() {
-    List<String> data = [];
+  // void htmlparser() {
+  //   List<String> data = [];
 
-    data.add(object.description != null && object.description != ""
-        ? object.description["__cdata"]
-            .getElementsByClassName("time")[0]
-            .innerHtml
-        : "");
+  //   data.add(object.description != null && object.description != ""
+  //       ? object.description["__cdata"]
+  //           .getElementsByClassName("time")[0]
+  //           .innerHtml
+  //       : "");
 
-    final temp = object.description != null && object.description != ""
-        ? object.description["__cdata"].getElementsByClassName("temp")[0]
-        : "";
-    data.add(temp.innerHtml.substring(0, temp.innerHtml.indexOf("<span>")));
-    data.add(temp
-        .getElementsByTagName("small")[0]
-        .innerHtml
-        .replaceAll(RegExp("[(|)|℃]"), ""));
+  //   final temp = object.description != null && object.description != ""
+  //       ? object.description["__cdata"].getElementsByClassName("temp")[0]
+  //       : "";
+  //   data.add(temp.innerHtml.substring(0, temp.innerHtml.indexOf("<span>")));
+  //   data.add(temp
+  //       .getElementsByTagName("small")[0]
+  //       .innerHtml
+  //       .replaceAll(RegExp("[(|)|℃]"), ""));
 
-    final rows = object.description != null && object.description != ""
-        ? object.description["__cdata"]
-            .getElementsByTagName("table")[0]
-            .getElementsByTagName("td")
-        : "";
+  //   final rows = object.description != null && object.description != ""
+  //       ? object.description["__cdata"]
+  //           .getElementsByTagName("table")[0]
+  //           .getElementsByTagName("td")
+  //       : "";
 
-    rows.map((e) => e.innerHtml).forEach((element) {
-      if (element != "-") {
-        data.add(element);
-      }
-    });
-  }
+  //   rows.map((e) => e.innerHtml).forEach((element) {
+  //     if (element != "-") {
+  //       data.add(element);
+  //     }
+  //   });
+  // }
 
   Widget _buildItem(BuildContext context) {
     return RefreshIndicator(
