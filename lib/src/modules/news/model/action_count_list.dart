5616e6ed27f1d904235f621c9b702d6ea30d01e8
Zehntech <zehntech@Zehntechs-Mac-mini-2.local>
2022-01-31 17:46:25 +0530

    small

--- lib/src/modules/news/model/action_count_list.dart
+++ lib/src/modules/news/model/action_count_list.dart
@@ -32,18 +32,18 @@ class ActionCountList {
 
         notificationId: json['Notification_Id__c'] as String?,
         schoolId: json['School_App__c'] as String?,
-        likeCount: json['Like__c'] as int?,
-        thanksCount: json['Thanks__c'] as int?,
-        helpfulCount: json['Helpful__c'] as int?,
-        shareCount: json['Share__c'] as int?,
+        likeCount: json['Total_Likes__c'] as int?,
+        thanksCount: json['Total_Thanks__c'] as int?,
+        helpfulCount: json['Total_Helpful__c'] as int?,
+        shareCount: json['Total_Share__c'] as int?,
       );
 
   Map<String, dynamic> toJson() => {
         'Notification_Id__c': notificationId,
         'School_App__c': schoolId,
-        'Like__c': likeCount,
-        'Thanks__c': thanksCount,
-        'helpful': helpfulCount,
-        'Share__c': shareCount,
+        'Total_Likes__c': likeCount,
+        'Total_Thanks__c': thanksCount,
+        'Total_Helpful__c': helpfulCount,
+        'Total_Share__c': shareCount,
       };
 }
