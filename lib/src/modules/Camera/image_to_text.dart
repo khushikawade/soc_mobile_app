5755509ec1ac2535990d24b957b831481d4d7c8c
rupesh.parmar+1-at-547321245783 <mona.zehntech@gmail.com>
2022-03-23 12:25:47 +0530

    general push

--- lib/src/modules/Camera/image_to_text.dart
+++ lib/src/modules/Camera/image_to_text.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
@@ -16,6 +18,9 @@ Future<void>? cameravalue;
 
 class _ImageToTextState extends State<ImageToText> {
   String pathOfImage = '';
+  String finalText = ' ';
+  bool isLoaded = false;
+  late File myImagePath;
   @override
   void initState() {
     super.initState();
@@ -39,61 +44,110 @@ class _ImageToTextState extends State<ImageToText> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Image To Text'),
-        ),
-        body: Column(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
+            const SizedBox(
               height: 100,
             ),
             Container(
               height: MediaQuery.of(context).size.height * 0.5,
               width: MediaQuery.of(context).size.width,
+              color: Colors.teal,
+              child: isLoaded
+                  ? Image.file(
+                      myImagePath,
+                      fit: BoxFit.fill,
+                    )
+                  : Text("This is image section "),
             ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Pick Image'),
-            ),
-            Text('this is extended data')
-          ],
-        )
-        // Container(
-        //   height: MediaQuery.of(context).size.height,
-        //   width: MediaQuery.of(context).size.width,
-        //   child: Center(
-        //     child: FutureBuilder(
-        //         future: cameravalue,
-        //         builder: (context, snapshot) {
-        //           if (snapshot.connectionState == ConnectionState.done) {
-        //             return controller == null
-        //                 ? Container()
-        //                 : CameraPreview(controller!);
-        //           } else {
-        //             return Center(
-        //               child: CircularProgressIndicator(),
-        //             );
-        //           }
-        //         }),
-        //   ),
-        // ),
+            Center(
+                child: Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    getImage();
 
-        );
+                    // Future.delayed(Duration(seconds: 15), () {
+                    //   reconizeText(pathOfImage);
+                    // });
+                  },
+                  child: Text(
+                    "gallary Image",
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    getCameraImage();
+
+                    // Future.delayed(Duration(seconds: 15), () {
+                    //   reconizeText(pathOfImage);
+                    // });
+                  },
+                  child: Text(
+                    "Camera Image",
+                  ),
+                ),
+              ],
+            )),
+            Text(
+              finalText != null ? finalText : "This is my text",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   // image pickup from gallary
   void getImage() async {
     ImagePicker _imagePicker = ImagePicker();
     XFile? image = await _imagePicker.pickImage(source: ImageSource.gallery);
+
     setState(() {
-      pathOfImage = image!.path;
+      myImagePath = File(image!.path);
+      isLoaded = true;
+      pathOfImage = image.path.toString();
     });
+    reconizeText(pathOfImage);
+  }
+
+  void getCameraImage() async {
+    ImagePicker _imagePicker = ImagePicker();
+    XFile? image = await _imagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      myImagePath = File(image!.path);
+      isLoaded = true;
+      pathOfImage = image.path.toString();
+    });
+    reconizeText(pathOfImage);
   }
 
   // Function for recongnising text
   Future<void> reconizeText(path) async {
-    final inputImage = await InputImage.fromFilePath(path);
-    final textDetector = GoogleMlKit.vision.textDetector();
-    final RecognisedText _recognisedText = await  textDetector.processImage(inputImage);
+    try {
+      finalText = '';
+      final inputImage = InputImage.fromFilePath(path);
+      final textDetector = GoogleMlKit.vision.textDetectorV2();
+      //.vision.textDetector();
+      final RecognisedText _recognisedText =
+          await textDetector.processImage(inputImage);
+      for (TextBlock block in _recognisedText.blocks) {
+        for (TextLine textLine in block.lines) {
+          for (TextElement textElement in textLine.elements) {
+            setState(() {
+              finalText = finalText + " " + textElement.text;
+            });
+          }
+          finalText = finalText + '\n';
+        }
+      }
+    } catch (e) {}
   }
+
+  
 }
