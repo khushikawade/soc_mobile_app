7d1844556a0c85ac89e7dd6570ef5ea68bb6583f
Zehntech <zehntech@Zehntechs-Mac-mini-2.local>
2022-03-30 19:49:07 +0530

    general changes

--- lib/src/modules/custom/ui/open_external.dart
+++ lib/src/modules/custom/ui/open_external.dart
@@ -17,12 +17,16 @@ class UrlNotSecure extends StatefulWidget {
     required this.url,
   }) : super(key: key);
 
+ 
   @override
   State<UrlNotSecure> createState() => _UrlNotSecureState();
+  
 }
+
 
 class _UrlNotSecureState extends State<UrlNotSecure> {
   Widget build(BuildContext context) {
+    
     return widget.connected == false
         ? NoInternetErrorWidget(
             issplashscreen: false,
@@ -48,13 +52,35 @@ class _UrlNotSecureState extends State<UrlNotSecure> {
                     )),
                 SpacerWidget(12),
                 Container(
-                    margin: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height * 0.35),
-                    alignment: Alignment.center,
-                    child: ElevatedButton(
-                      child: Text('aahjsdags'),
-                      onPressed: () {},
-                    ))
+        padding: EdgeInsets.all(_kPadding / 6),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisSize: MainAxisSize.,
+          children: <Widget>[
+            Container(
+              constraints: BoxConstraints(
+                minWidth: _KButtonSize,
+                maxWidth: 230.0,
+                minHeight: _KButtonSize / 2,
+                maxHeight: _KButtonSize / 2,
+              ),
+              child: ElevatedButton(
+                onPressed: ()  {
+                  launchUrl(widget.url);
+                },
+                child: TranslationWidget(
+                  message: "Open in external browser",
+                  toLanguage: Globals.selectedLanguage,
+                  fromLanguage: "en",
+                  builder: (translatedMessage) => Text(
+                    translatedMessage.toString(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
               ],
             );
           });
@@ -63,4 +89,8 @@ class _UrlNotSecureState extends State<UrlNotSecure> {
   launchUrl(url) async {
     await Utility.launchUrlOnExternalBrowser(url);
   }
+  static const double _kPadding = 16.0;
+  static const double _KButtonSize = 110.0;
 }
+
+
