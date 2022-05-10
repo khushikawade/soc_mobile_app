b918bfc5ba7f410e76d23a417121aea3aee29678
Devesh Gupta <50574293+ztdg0068@users.noreply.github.com>
2022-05-10 11:58:50 +0530

    push

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,9 +5,7 @@
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:camera_web/camera_web.dart';
 import 'package:connectivity_for_web/connectivity_for_web.dart';
-import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:package_info_plus_web/package_info_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -19,9 +17,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  CameraPlugin.registerWith(registrar);
   ConnectivityPlugin.registerWith(registrar);
-  GoogleSignInPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
   PackageInfoPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
