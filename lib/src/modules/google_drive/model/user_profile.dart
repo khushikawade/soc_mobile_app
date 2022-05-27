cb4d061fb299f7779430c3ca43062f39aaa1579f
Devesh Gupta <50574293+ztdg0068@users.noreply.github.com>
2022-05-27 11:02:55 +0530

    pull

---
+++ lib/src/modules/google_drive/model/user_profile.dart
@@ -0,0 +1,14 @@
+// class User {
+//   String? title;
+//   String? description;
+//   String? fileid;
+//   final label;
+//   HistoryAssessment({this.title, this.description, this.fileid, this.label});
+
+//   factory HistoryAssessment.fromJson(Map<String, dynamic> json) => HistoryAssessment(
+//       title: json['title'] as String?,
+//       description: json['description'] as String?,
+//       fileid: json['id'] as String?,
+//       label: json['labels'] 
+//       );
+// }
