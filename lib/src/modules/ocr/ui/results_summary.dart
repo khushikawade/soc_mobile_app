f5b69ff688ba520dde2a8b8b09ac5f8a6ddce1ba
unknown <nikhar>
2022-06-27 16:22:03 +0530

    edit and delete

--- lib/src/modules/ocr/ui/results_summary.dart
+++ lib/src/modules/ocr/ui/results_summary.dart
@@ -709,7 +709,7 @@ class _ResultsSummaryState extends State<ResultsSummary> {
         itemCount: _list.length, // Globals.gradeList.length,
         itemBuilder: (BuildContext context, int index) {
           return Slidable(
-              enabled: widget.assessmentDetailPage!
+              enabled: widget.assessmentDetailPage== true
                   ? false
                   : (Globals.scanMoreStudentInfoLength ?? -1) <= index
                       ? true
