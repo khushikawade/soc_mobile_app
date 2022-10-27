5572a29eabd2b8da82721515d948457d6354efcc
Apple <apple@Apples-MacBook-Pro.local>
2022-10-27 16:21:06 +0530

    push

--- lib/src/modules/google_drive/model/spreadsheet_model.dart
+++ lib/src/modules/google_drive/model/spreadsheet_model.dart
@@ -28,21 +28,10 @@ class ResultSpreadsheet {
       this.customRubricImage,
       this.assessmentImage);
 
-  ResultSpreadsheet.fromList(List items, bool? isStandalone)
-      : this(
-            items[0],
-            items[1],
-            items[2],
-            items[3],
-            isStandalone! ? items[12] : items[4],
-            items[5],
-            items[6],
-            items[7],
-            items[8],
-            items[9],
-            items[10],
-            items[11],
-            isStandalone ? items[4] : items[12]);
+  ResultSpreadsheet.fromList(
+    List items,
+  ) : this(items[0], items[1], items[2], items[3], items[4], items[5], items[6],
+            items[7], items[8], items[9], items[10], items[11], items[12]);
 
   @override
   String toString() {
