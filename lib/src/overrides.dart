e47e45e470feed3d770cf680357e81b04a7e350e
Jyotsna Andel <jyotsna.andel@zehntech.com>
2023-01-25 19:53:07 +0530

    pull

--- lib/src/overrides.dart
+++ lib/src/overrides.dart
@@ -37,8 +37,8 @@ class Overrides {
       'https://anl2h22jc4.execute-api.us-east-2.amazonaws.com/production/classroom-login/auth';
 
 //true in case of graded+ stand-alone app
-  static bool STANDALONE_GRADED_APP = true;
-  static String SCHOOL_ID = 'a1f4W000008SzlXQAS';
+  static bool STANDALONE_GRADED_APP = false;
+  static String SCHOOL_ID = 'a1f4W000007DQaNQAW';
 
   //151 : 'a1f4W000007DQaNQAW'
   //Bronx Bears : 'a1f4W000007DR8nQAG';
