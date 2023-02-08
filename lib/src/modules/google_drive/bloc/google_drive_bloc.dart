d953297e2b570f7888227e3ce1a3b97c0db80586
Jyotsna Andel <jyotsna.andel@zehntech.com>
2023-02-08 19:08:57 +0530

    push

--- lib/src/modules/google_drive/bloc/google_drive_bloc.dart
+++ lib/src/modules/google_drive/bloc/google_drive_bloc.dart
@@ -2511,15 +2511,13 @@ class GoogleDriveBloc extends Bloc<GoogleDriveEvent, GoogleDriveState> {
   Future<List<Map>> _getListOfAssignmentDetails(
       {required String? assignmentName,
       required StudentAssessmentInfo studentAssessmentInfoObj}) async {
-    // List of title for slide details table
-    // List<String> listOfFields = [
-    //   'Subject',
-    //   'Grade',
-    //   'Class',
-    //   'Domain',
-    //   'Sub-Domain'
-    // ];
+    // check if question image is available or not
+    bool? isQuestionImgUrlUpdate =
+        studentAssessmentInfoObj.questionImgUrl != null &&
+            studentAssessmentInfoObj.questionImgUrl!.isNotEmpty &&
+            studentAssessmentInfoObj.questionImgUrl! != "NA";
 
+    // List of title for slide details table
     List<String> listOfFields = [
       'Assignment name',
       'Subject',
@@ -2554,7 +2552,7 @@ class GoogleDriveBloc extends Bloc<GoogleDriveEvent, GoogleDriveState> {
             "transform": {
               "scaleX": 1,
               "scaleY": 1,
-              "translateX": 3005000,
+              "translateX": isQuestionImgUrlUpdate ? 3005000 : 1505000,
               "translateY": 280000,
               "unit": "EMU",
             }
@@ -2564,9 +2562,7 @@ class GoogleDriveBloc extends Bloc<GoogleDriveEvent, GoogleDriveState> {
     ];
 
 // check if question image is available or not
-    if (studentAssessmentInfoObj.questionImgUrl != null &&
-        studentAssessmentInfoObj.questionImgUrl! != "NA" &&
-        studentAssessmentInfoObj.questionImgUrl!.isNotEmpty) {
+    if (isQuestionImgUrlUpdate) {
 //adding question image on slide request
       body.add({
         "createImage": {
