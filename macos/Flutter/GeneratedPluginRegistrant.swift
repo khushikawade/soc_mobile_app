7b64dc3e558f4e24f6a82f543cd000df6b74256a
Jyotsna Andel <jyotsna.andel@zehntech.com>
2023-02-08 21:46:22 +0530

    pull

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,19 +8,12 @@ import Foundation
 import connectivity_plus_macos
 import firebase_analytics
 import firebase_core
-<<<<<<< HEAD
-=======
 import firebase_crashlytics
->>>>>>> graded_plus_main
 import geolocator_apple
 import network_info_plus_macos
 import package_info_plus_macos
 import path_provider_macos
-<<<<<<< HEAD
-import shared_preferences_macos
-=======
 import shared_preferences_foundation
->>>>>>> graded_plus_main
 import sqflite
 import url_launcher_macos
 import wakelock_macos
@@ -29,10 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-<<<<<<< HEAD
-=======
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
->>>>>>> graded_plus_main
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
