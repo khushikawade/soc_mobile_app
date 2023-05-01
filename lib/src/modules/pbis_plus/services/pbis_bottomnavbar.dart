3860c48ce183808dba13c840af1a1cad87403a6e
Jyotsna Andel <jyotsna.andel@zehntech.com>
2023-05-01 12:49:08 +0530

    pull

--- lib/src/modules/pbis_plus/services/pbis_bottomnavbar.dart
+++ lib/src/modules/pbis_plus/services/pbis_bottomnavbar.dart
@@ -1,5 +1,4 @@
 import 'package:Soc/src/globals.dart';
-import 'package:Soc/src/modules/pbis_plus/services/pbis_plus_icons.dart';
 import 'package:Soc/src/modules/pbis_plus/ui/pbis_plus_class_section/pbis_plus_class.dart';
 import 'package:Soc/src/modules/pbis_plus/ui/pbis_plus_history_section.dart/pbis_plus_history.dart';
 import 'package:Soc/src/overrides.dart';
