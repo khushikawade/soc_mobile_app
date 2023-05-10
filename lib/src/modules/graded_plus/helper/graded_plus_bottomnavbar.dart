007c24dce36ea20fbe6c4a96cb781f5a553dae7b
Nikhar Tale <nikhar.tale@zehntech.com>
2023-05-10 15:43:56 +0530

    pulled

---
+++ lib/src/modules/graded_plus/helper/graded_plus_bottomnavbar.dart
@@ -0,0 +1,115 @@
+import 'package:Soc/src/globals.dart';
+import 'package:Soc/src/modules/pbis_plus/ui/pbis_plus_staff.dart';
+import 'package:Soc/src/overrides.dart';
+import 'package:Soc/src/styles/theme.dart';
+import 'package:Soc/src/translator/translation_widget.dart';
+import 'package:Soc/src/widgets/spacer_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+class GradedBottomNavBar {
+  /* ------- function to return list of widget for bottom navigation bar ------ */
+  static List<Widget> gradedPlusBuildPersistentScreens() {
+    return [
+      PBISPlusStaff(
+        titleIconData: getStaffIconCode(),
+      ),
+      PBISPlusStaff(
+        titleIconData: getStaffIconCode(),
+      ),
+      PBISPlusStaff(
+        titleIconData: getStaffIconCode(),
+      )
+    ];
+  }
+
+  /* ---------- function to return list of bottom navigation bar item --------- */
+  static List<PersistentBottomNavBarItem> gradedPlusnavBarsItems(
+      {required BuildContext context}) {
+    return [
+      PersistentBottomNavBarItem(
+        icon: gardedPlusBottomNavBarIcons(
+            IconData(0xe825,
+                fontFamily: Overrides.kFontFam,
+                fontPackage: Overrides.kFontPkg),
+            'Class',
+            context),
+        activeColorPrimary: AppTheme.kButtonColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: gardedPlusBottomNavBarIcons(
+            IconData(0xe824,
+                fontFamily: Overrides.kFontFam,
+                fontPackage: Overrides.kFontPkg),
+            'History',
+            context),
+        activeColorPrimary: AppTheme.kButtonColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: gardedPlusBottomNavBarIcons(getStaffIconCode(), 'Staff', context),
+        activeColorPrimary: AppTheme.kButtonColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      )
+    ];
+  }
+
+  /* -------------------- bottom navigation bar item widget ------------------- */
+  static Widget gardedPlusBottomNavBarIcons(IconData iconData, title, context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                iconData,
+                size: 22,
+              ),
+              SpacerWidget(2),
+              TranslationWidget(
+                shimmerHeight: 8,
+                message: title,
+                fromLanguage: "en",
+                toLanguage: Globals.selectedLanguage,
+                builder: (translatedMessage) => Expanded(
+                  child: FittedBox(
+                    child: Text(
+                      translatedMessage.toString(),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: Theme.of(context).textTheme.headline4!,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  /* --------- function to get staff icon code from main bottom navBar -------- */
+  static IconData getStaffIconCode() {
+    String staffIconCode = '0xe808';
+    IconData iconData = IconData(int.parse(staffIconCode),
+        fontFamily: Overrides.kFontFam, fontPackage: Overrides.kFontPkg);
+    for (var i = 0;
+        i < Globals.appSetting.bottomNavigationC!.split(";").length;
+        i++) {
+      if (Globals.appSetting.bottomNavigationC!.split(";").contains('staff')) {
+        staffIconCode =
+            Globals.appSetting.bottomNavigationC!.split(";")[i].split("_")[1];
+
+        return IconData(int.parse(staffIconCode),
+            fontFamily: Overrides.kFontFam, fontPackage: Overrides.kFontPkg);
+      }
+    }
+    return iconData;
+  }
+}
---
+++ lib/src/modules/graded_plus/ui/graded_plus_navbar_home.dart
@@ -0,0 +1,80 @@
+import 'package:Soc/src/globals.dart';
+import 'package:Soc/src/modules/graded_plus/helper/graded_plus_bottomnavbar.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+class GradedPlusNavBarHome extends StatefulWidget {
+  const GradedPlusNavBarHome({
+    Key? key,
+  });
+
+  @override
+  State<GradedPlusNavBarHome> createState() => _GradedPlusNavBarHomeState();
+}
+
+class _GradedPlusNavBarHomeState extends State<GradedPlusNavBarHome> {
+  PersistentTabController? gradedPlusPersistentTabController;
+
+  @override
+  void initState() {
+    super.initState();
+    gradedPlusPersistentTabController =
+        PersistentTabController(initialIndex: 1);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    gradedPlusPersistentTabController!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: body(),
+    );
+  }
+
+  Widget body() {
+    return PersistentTabView(
+      context,
+      controller: gradedPlusPersistentTabController,
+      screens: GradedBottomNavBar.gradedPlusBuildPersistentScreens(),
+      onItemSelected: (i) {
+        if (i == 2) {
+          Navigator.of(context).pop();
+        }
+      },
+      items: GradedBottomNavBar.gradedPlusnavBarsItems(context: context),
+      confineInSafeArea: true,
+      backgroundColor: Theme.of(context).backgroundColor,
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: false, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(25),
+          topLeft: Radius.circular(25),
+        ),
+      ),
+      onWillPop: (context) async {
+        return false; // disable back button on android
+      },
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: ItemAnimationProperties(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        animateTabTransition: false,
+      ),
+      navBarStyle: NavBarStyle.style6,
+      navBarHeight: Globals.deviceType == "phone" ? 60 : 70,
+    );
+  }
+}
