5d74558909393415e49dee444ac43648ed4986cf
Jyotsna Andel <jyotsna.andel@zehntech.com>
2023-07-04 19:39:50 +0530

    sorting order improved

--- lib/src/modules/pbis_plus/bloc/pbis_plus_bloc.dart
+++ lib/src/modules/pbis_plus/bloc/pbis_plus_bloc.dart
@@ -1481,8 +1481,10 @@ class PBISPlusBloc extends Bloc<PBISPlusEvent, PBISPlusState> {
   List<PBISPlusCommonBehaviorModal> sortByOrder(
       List<PBISPlusCommonBehaviorModal> allBehaviors) {
     allBehaviors.sort((a, b) {
-      int orderA = int.parse(a.pBISBehaviorSortOrderC ?? '1');
-      int orderB = int.parse(b.pBISBehaviorSortOrderC ?? '1');
+      int orderA = int.parse(
+          a.pBISBehaviorSortOrderC != '' ? a.pBISBehaviorSortOrderC! : '1');
+      int orderB = int.parse(
+          b.pBISBehaviorSortOrderC != '' ? b.pBISBehaviorSortOrderC! : '1');
 
       return orderA.compareTo(orderB);
       // For descending order: return orderB.compareTo(orderA);
