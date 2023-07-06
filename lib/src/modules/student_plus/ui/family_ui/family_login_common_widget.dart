e1b0384d5ce24ef9a84e32ea22e9cbffeeb7eed2
APPLE <apple@APPLEs-Mac-mini.local>
2023-07-06 12:22:18 +0530

    done login screen

--- lib/src/modules/student_plus/ui/family_ui/family_login_common_widget.dart
+++ lib/src/modules/student_plus/ui/family_ui/family_login_common_widget.dart
@@ -1,3 +1,41 @@
+import 'package:Soc/src/services/utility.dart';
+import 'package:Soc/src/widgets/spacer_widget.dart';
+import 'package:flutter/material.dart';
+
 class FamilyLoginCommonWidget {
-  
+  /* ------------------ Widget to show title and description in family login flow ----------------- */
+  static Widget titleAndDesWidget(
+      {required BuildContext context,
+      required String title,
+      String? description}) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.15),
+      alignment: Alignment.center,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Utility.textWidget(
+            text: title,
+            context: context,
+            textTheme: Theme.of(context).textTheme.headline1),
+        SpacerWidget(10),
+        Utility.textWidget(
+            textAlign: TextAlign.center,
+            text: 'Enter your email below to receive a one-time password.',
+            context: context,
+            textTheme: Theme.of(context).textTheme.headline3)
+      ]),
+    );
+  }
+
+  static Widget familyCircularIcon({required BuildContext context, required String assetImageUrl}) {
+    return Container(
+      height: MediaQuery.of(context).size.width * 0.5,
+      // width: MediaQuery.of(context).size.width * 0.4,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/login_lock.png')),
+          color: Colors.black,
+          shape: BoxShape.circle),
+    );
+  }
 }
--- lib/src/modules/student_plus/ui/family_ui/student_plus_family_login.dart
+++ lib/src/modules/student_plus/ui/family_ui/student_plus_family_login.dart
@@ -1,14 +1,12 @@
+import 'package:Soc/src/globals.dart';
+import 'package:Soc/src/modules/graded_plus/widgets/common_fab.dart';
 import 'package:Soc/src/modules/plus_common_widgets/plus_background_img_widget.dart';
-import 'package:Soc/src/modules/student_plus/widgets/student_plus_app_bar.dart';
+import 'package:Soc/src/modules/student_plus/ui/family_ui/family_login_common_widget.dart';
 import 'package:Soc/src/overrides.dart';
 import 'package:Soc/src/services/utility.dart';
 import 'package:Soc/src/styles/theme.dart';
 import 'package:Soc/src/widgets/spacer_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class StudentPlusFamilyLogIn extends StatefulWidget {
   const StudentPlusFamilyLogIn({Key? key}) : super(key: key);
@@ -18,6 +16,7 @@ class StudentPlusFamilyLogIn extends StatefulWidget {
 }
 
 class _StudentPlusFamilyLogInState extends State<StudentPlusFamilyLogIn> {
+  TextEditingController emailEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -38,44 +37,127 @@ class _StudentPlusFamilyLogInState extends State<StudentPlusFamilyLogIn> {
               onPressed: () {},
             ),
           ),
-          body: ListView(
-            physics: BouncingScrollPhysics(),
-            children: [
-              SpacerWidget(MediaQuery.of(context).size.height * 0.1),
-              Container(
-                margin: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width * 0.15),
-                alignment: Alignment.center,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Utility.textWidget(
-                          text: 'Email',
-                          context: context,
-                          textTheme: Theme.of(context).textTheme.headline1),
-                      SpacerWidget(10),
-                      Utility.textWidget(
-                          textAlign: TextAlign.center,
-                          text:
-                              'Enter your email below to receive a one-time password.',
-                          context: context,
-                          textTheme: Theme.of(context).textTheme.headline3)
-                    ]),
-              ),
-              SpacerWidget(MediaQuery.of(context).size.height * 0.1),
-              Container(
-                height: MediaQuery.of(context).size.width * 0.42,
-                // width: MediaQuery.of(context).size.width * 0.4,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/login_lock.png')),
-                    color: Colors.black,
-                    shape: BoxShape.circle),
-              )
-            ],
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            child: ListView(
+              physics: BouncingScrollPhysics(),
+              children: [
+                SpacerWidget(MediaQuery.of(context).size.height * 0.1),
+                FamilyLoginCommonWidget.titleAndDesWidget(
+                    context: context,
+                    title: 'Email',
+                    description:
+                        'Enter your email below to receive a one-time password.'),
+                SpacerWidget(MediaQuery.of(context).size.height * 0.1),
+                FamilyLoginCommonWidget.familyCircularIcon(
+                    context: context,
+                    assetImageUrl: 'assets/images/login_lock.png'),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: Utility.textWidget(
+                      context: context,
+                      text: 'Enter your email',
+                      textTheme: Theme.of(context).textTheme.headline3),
+                ),
+                SpacerWidget(20),
+                textFormFieldWidget(),
+                SpacerWidget(10),
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    child: fabButton())
+              ],
+            ),
           ),
         ),
       ],
     );
+  }
+
+  /* ------------------------ Widget to get student email (Text form field) ----------------------- */
+  Widget textFormFieldWidget() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: TextFormField(
+        autofocus: false,
+        style: Theme.of(context).textTheme.headline3,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        controller: emailEditingController,
+        cursorColor: Theme.of(context).colorScheme.primaryVariant,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 14, horizontal: 0),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              borderSide: BorderSide(color: AppTheme.kButtonColor, width: 1),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              borderSide: BorderSide(color: Colors.red, width: 1),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              borderSide: BorderSide(color: Colors.red, width: 1),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              borderSide: BorderSide(color: Colors.grey, width: 1),
+            ),
+            hintStyle: Theme.of(context)
+                .textTheme
+                .headline3!
+                .copyWith(fontWeight: FontWeight.w300, color: Colors.grey),
+            hintText: 'Email',
+            fillColor: Color(0xff000000) != Theme.of(context).backgroundColor
+                ? Theme.of(context).colorScheme.secondary
+                : Color.fromARGB(255, 12, 20, 23),
+            //Theme.of(context).colorScheme.secondary,
+
+            prefixIcon: Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Icon(
+                Icons.email,
+                size: 26,
+                color: AppTheme.kButtonColor,
+              ),
+            ),
+            suffixIcon: IconButton(
+              onPressed: () {
+                emailEditingController.clear();
+              },
+              icon: Icon(
+                Icons.clear,
+                color: Theme.of(context).colorScheme.primaryVariant,
+                size: Globals.deviceType == "phone" ? 20 : 28,
+              ),
+            ),
+            prefix: SizedBox(
+              width: 20,
+            )),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your email';
+          }
+          if (!isValidEmail(value)) {
+            return 'Please enter a valid email';
+          }
+          return null;
+        },
+        onChanged: (value) {},
+      ),
+    );
+  }
+
+  /* ----------------------------- widget to show generate otp button ----------------------------- */
+  Widget fabButton() {
+    return GradedPlusCustomFloatingActionButton(
+      isExtended: true,
+      fabWidth: MediaQuery.of(context).size.width * 0.7,
+      title: 'Generate OTP',
+      onPressed: () async {},
+    );
+  }
+
+  static bool isValidEmail(String value) {
+    final emailRegex = RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+    return emailRegex.hasMatch(value);
   }
 }
