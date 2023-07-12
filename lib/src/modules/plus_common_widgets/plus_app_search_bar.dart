86d769ad3fc77529833894dc57448be3d2a95f9a
APPLE <apple@APPLEs-Mac-mini.local>
2023-07-12 18:54:44 +0530

    making field readonlt

--- lib/src/modules/plus_common_widgets/plus_app_search_bar.dart
+++ lib/src/modules/plus_common_widgets/plus_app_search_bar.dart
@@ -104,6 +104,7 @@ class _PlusAppSearchBarState extends State<PlusAppSearchBar> {
 
   Widget innerSearchBar(String translatedMessage) {
     return TextFormField(
+        readOnly: true,
         autofocus: widget.autoFocus,
         style: Theme.of(context).textTheme.headline5,
         focusNode: widget.focusNode,
