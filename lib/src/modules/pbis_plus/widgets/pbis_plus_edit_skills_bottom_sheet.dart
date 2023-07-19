7a3d6f5a443df7192d4bc41c98d11da9030ca5be
Zehntech-SOC-M1-Mac-Mini-2020 <zehntech@Zehntech-SOC-M1-Mac-Mini-2022.local>
2023-07-19 17:53:03 +0530

    fix the isssue

--- lib/src/modules/pbis_plus/widgets/pbis_plus_edit_skills_bottom_sheet.dart
+++ lib/src/modules/pbis_plus/widgets/pbis_plus_edit_skills_bottom_sheet.dart
@@ -17,7 +17,7 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 class PBISPlusEditSkillsBottomSheet extends StatefulWidget {
   final double? height;
   final PBISPlusCommonBehaviorModal? item;
-  BoxConstraints? constraints;
+  double? constraints;
   final int? index = -1;
   final VoidCallback onDelete;
   final void Function(String) onEditCallBack;
@@ -26,7 +26,7 @@ class PBISPlusEditSkillsBottomSheet extends StatefulWidget {
       {Key? key,
       this.height = 100,
       required this.item,
-      required BoxConstraints constraints,
+      required  this.constraints,
       required int index,
       required this.onDelete,
       required this.onEditCallBack});
@@ -77,7 +77,9 @@ class _PBISPlusBottomSheetState extends State<PBISPlusEditSkillsBottomSheet> {
             ),
             height: pageValue == 0
                 ? widget.height
-                : MediaQuery.of(context).size.height * 0.4,
+                :(widget.constraints!<=700)?
+                MediaQuery.of(context).size.height * 0.49:
+                 MediaQuery.of(context).size.height * 0.4,
             //saveAndShareOptions
             child: PageView(
                 physics: NeverScrollableScrollPhysics(),
@@ -248,7 +250,7 @@ class _PBISPlusBottomSheetState extends State<PBISPlusEditSkillsBottomSheet> {
     return Container(
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.only(
-            left: 30, right: 30, bottom: widget.height! * 0.16, top: 18),
+            left: 40, right: 40, bottom: 18, top: 18),
         child: FloatingActionButton.extended(
             backgroundColor: AppTheme.kButtonColor.withOpacity(1.0),
             onPressed: () async {
