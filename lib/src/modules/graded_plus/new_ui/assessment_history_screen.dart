76c98ee00e0351489e78a1aa4e876f00debfbd92
Rupesh Parmar <rupesh.parmar@zehntech.com>
2023-08-07 12:15:54 +0530

    changes

--- lib/src/modules/graded_plus/new_ui/assessment_history_screen.dart
+++ lib/src/modules/graded_plus/new_ui/assessment_history_screen.dart
@@ -349,7 +349,7 @@ class _GradedPlusAssessmentSummaryState
           ? MediaQuery.of(context).size.height * 0.792
           : MediaQuery.of(context).size.height * 0.45,
       child: ListView.builder(
-        controller: _scrollController,
+        controller: _list.length > 5 ? _scrollController : null,
         shrinkWrap: true,
         // padding: EdgeInsets.only(bottom: AppTheme.klistPadding),
         scrollDirection: Axis.vertical,
